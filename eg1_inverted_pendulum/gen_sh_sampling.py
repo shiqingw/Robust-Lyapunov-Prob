857d563eb6ee057de543b3433e2f52829b8e3249
Shiqing Wei <shiqing.wei@outlook.com>
2025-03-27 12:58:44 -0400

    backup

--- eg1_inverted_pendulum/gen_sh_sampling.py
+++ eg1_inverted_pendulum/gen_sh_sampling.py
@@ -3,25 +3,27 @@ import os
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
-def generate_sh_script_sampling_solve_on_nom(filename, exp_nums, delta, epsilon):
-    with open(filename, "w") as file:
+def generate_sh_script_sampling_solve_on_nom(filename, exp_nums, device, delta, epsilon):
+    with open(filename, "a") as file:
         for exp_num in exp_nums:
             command1 = f"mkdir eg1_results/{exp_num:03d}\n"
             command2 = f"python -u eg1_inverted_pendulum/sampling_on_nom.py --exp_num {exp_num} " + \
+                f"--device {device} " + \
                 f"--delta {delta:.1E} " + \
                 f"--epsilon {epsilon:.1E} " + \
-                f"> eg1_results/{exp_num:03d}/output_sampling_on_nom_delta_{delta:.1E}_epsilon_{epsilon:.1E}.out\n"
+                f"> eg1_results/{exp_num:03d}/output_sampling_on_nom_{device}_delta_{delta:.1E}_epsilon_{epsilon:.1E}.out\n"
             file.write(command1)
             file.write(command2)
 
-def generate_sh_script_sampling_solve_on_true(filename, exp_nums, delta, epsilon):
+def generate_sh_script_sampling_solve_on_true(filename, exp_nums, device, delta, epsilon):
     with open(filename, "a") as file:
         for exp_num in exp_nums:
             command1 = f"mkdir eg1_results/{exp_num:03d}\n"
             command2 = f"python -u eg1_inverted_pendulum/sampling_on_true.py --exp_num {exp_num} " + \
+                f"--device {device} " + \
                 f"--delta {delta:.1E} " + \
                 f"--epsilon {epsilon:.1E} " + \
-                f"> eg1_results/{exp_num:03d}/output_sampling_on_true_delta_{delta:.1E}_epsilon_{epsilon:.1E}.out\n"
+                f"> eg1_results/{exp_num:03d}/output_sampling_on_true_{device}_delta_{delta:.1E}_epsilon_{epsilon:.1E}.out\n"
             file.write(command1)
             file.write(command2)
 
@@ -30,8 +32,16 @@ file = os.path.join(str(Path(__file__).parent.parent), f"run_sampling_solve.sh")
 delta = 1e-3
 epsilon = 1e-3
 
+device = "cuda"
 exp_nums = [1]
-generate_sh_script_sampling_solve_on_nom(file, exp_nums, delta, epsilon)
+generate_sh_script_sampling_solve_on_nom(file, exp_nums, device, delta, epsilon)
 
 exp_nums = [1,2]
-generate_sh_script_sampling_solve_on_true(file, exp_nums, delta, epsilon)
+generate_sh_script_sampling_solve_on_true(file, exp_nums, device, delta, epsilon)
+
+device = "cpu"
+exp_nums = [1]
+generate_sh_script_sampling_solve_on_nom(file, exp_nums, device, delta, epsilon)
+
+exp_nums = [1,2]
+generate_sh_script_sampling_solve_on_true(file, exp_nums, device, delta, epsilon)
